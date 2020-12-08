05e6344fceec53c2ea5f3cd830927cffe855203d
BenRamsden30 <Ben-Ramsden@hotmail.com>
2020-12-08 18:36:41 +0000

    Test project created, TODO: Tests

---
+++ SuppliersUnitTestProject/SuppliersTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuppliersUnitTestProject
+{
+    [TestClass]
+    public class SuppliersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
